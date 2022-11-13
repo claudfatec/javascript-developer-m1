c4f33f5fb40b7f2aabc1e87d7ecb2aea847f1724
claudfatec <claudfatec@gmail.com>
2022-11-13 00:58:12 -0300

    color pattern on VsCode - new file:   .vscode/settings.json 	modified:   06 - listas (arrays)/exercicios.md 	modified:   06 - listas (arrays)/resolucao.js

---
+++ 06 - listas (arrays)/resolucao.js
@@ -0,0 +1,15 @@
+//exerc. 1
+num = 7;
+for (let i = 1; i <= 10; i++) {
+    console.log(i*num);
+}
+
+//exerc. 2
+num = [2, 4, 136, 5, 6, 7, 8];
+for (let i = 0; i < num.length; i++) {
+    if (num[i]  % 2 == 0) {
+        console.log(num[i]);
+    }
+}
+
+// exerc.3
